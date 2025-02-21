e61857b2a8a233a0288078bc50730b7696afd6fb
j4ndrw <murjan_andreialin@yahoo.com>
2025-02-22 01:05:31 +0200

    refactor(game/math): removed the inverse lerp - it was a stupid idea to begin with

--- src/game/math/lerp.ts
+++ src/game/math/lerp.ts
@@ -1,7 +1,1 @@
-interface Lerp {
-  (a: number, b: number, t: number): number;
-  inverse(a: number, b: number, t: number): number;
-}
-
-export const lerp: Lerp = ((a, b, t) => a + (b - a) * t) as Lerp;
-lerp.inverse = (a, b, t) => (b - a) * t - a;
+export const lerp = (a: number, b: number, t: number) => a + (b - a) * t;
